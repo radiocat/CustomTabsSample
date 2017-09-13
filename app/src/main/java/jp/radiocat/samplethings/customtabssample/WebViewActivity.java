83bbf5e13c21f8d4606fe79ac79d120f530578af
radiocat <radiocat.app@gmail.com>
2017-09-14 00:55:10 +0900

    Initial commit

---
+++ app/src/main/java/jp/radiocat/samplethings/customtabssample/WebViewActivity.java
@@ -0,0 +1,24 @@
+package jp.radiocat.samplethings.customtabssample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(getString(R.string.load_url));
+    }
+}
